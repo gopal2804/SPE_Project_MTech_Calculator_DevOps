5b4af0f3bc8450ae673e071251238b0918d7e97f
gopal gupta <gopalgupta2804@gmail.com>
2023-03-25 20:20:28 +0530

    change in calculator codee

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -136,6 +136,7 @@ public class Calculator {
                     System.out.println("--------------------------------------------");
                     System.out.println("--------------------------------------------");
                     System.out.println("--------------------------------------------");
+
                     input.close();
                     return;
 
